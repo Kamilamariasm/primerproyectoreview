d47bc1590093a78b5a76fa6c08bc3d0a900a020c
Kamila Sabori <a22311054@uthermosillo.edu.mx>
2024-01-11 10:40:44 -0700

    tablas realizadas, tablas migradas, deshicimos migración con rollback en la tabla users y edité su esquema y migré

---
+++ api/larav/database/migrations/2024_01_11_163716_create_reviews_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->integer('schedules_id');
+            $table->integer('locations_id');
+            $table->integer('joints_id');
+            $table->integer('owners_id');
+            $table->string('image')->default('postholder.png');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
---
+++ api/larav/database/migrations/2024_01_11_172539_update_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('surname')->after('name');
+            $table->string('phone')->after('email');
+            $table->string('image')->default('placeholder.png')->after ('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('surname');
+            $table->dropColumn('phone');
+            $table->dropColumn('image');
+            });
+    }
+};
